09a1335ed2a32e84bf28c75ec12045d562da2c02
Elias Bermudez <eliasbermudezxiii@gmail.com>
2023-01-30 13:48:03 +0000

    Test

--- src/app.py
+++ src/app.py
@@ -10,7 +10,7 @@ from utils import APIException, generate_sitemap
 from admin import setup_admin
 from models import db, User, Characters, Planets, Vehicles, Favorites
 #from models import Person
-
+#$
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
