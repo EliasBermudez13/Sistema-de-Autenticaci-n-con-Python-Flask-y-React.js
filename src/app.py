34c75f10a85e64fa75d68b58686fd8fcff48adaa
Elias Bermudez <eliasbermudezxiii@gmail.com>
2023-01-30 19:35:55 +0000

    Backends Routes made

--- src/app.py
+++ src/app.py
@@ -9,7 +9,10 @@ from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
 from models import db, User, Characters, Planets, Vehicles, Favorites
-#from models import Person
+from flask_jwt_extended import create_access_token
+from flask_jwt_extended import get_jwt_identity
+from flask_jwt_extended import jwt_required
+from flask_jwt_extended import JWTManager
 #$
 app = Flask(__name__)
 app.url_map.strict_slashes = False
@@ -26,6 +29,10 @@ db.init_app(app)
 CORS(app)
 setup_admin(app)
 
+# Setup the Flask-JWT-Extended extension
+app.config["JWT_SECRET_KEY"] = "super-secret"  # Change this!
+jwt = JWTManager(app)
+
 # Handle/serialize errors like a JSON object
 @app.errorhandler(APIException)
 def handle_invalid_usage(error):
@@ -35,6 +42,32 @@ def handle_invalid_usage(error):
 @app.route('/')
 def sitemap():
     return generate_sitemap(app)
+
+# Create a route to authenticate your users and return JWTs. The
+# create_access_token() function is used to actually generate the JWT.
+@app.route("/login", methods=["POST"])
+def login():
+    email = request.json.get("email", None)
+    password = request.json.get("password", None)
+    user = User.query.filter_by(email=email).first()
+    print(user)
+    if user is None:
+        return jsonify({"msg": "User does not exists"}), 404
+    print(user.password)    
+    if password != user.password:
+        return jsonify({"msg": "Wrong password"}), 404
+
+    access_token = create_access_token(identity=email)
+    return jsonify(access_token=access_token)
+
+# Protect a route with jwt_required, which will kick out requests
+# without a valid JWT present.
+@app.route("/profile", methods=["GET"])
+@jwt_required()
+def get_profile():
+    # Access the identity of the current user with get_jwt_identity
+    current_user = get_jwt_identity()
+    return jsonify(logged_in_as=current_user), 200
 
 #get user
 @app.route('/user', methods=['GET'])
@@ -138,13 +171,13 @@ def add_vehicle_favorite(user_id, vehicle_id):
     results = list(map(lambda item: item.serialize(),favorites))
     return jsonify(results), 201
 
-@app.route('/user', methods=['POST'])
-def add_new_user():
-    allusers = User.query.all()
-    results = list(map(lambda item: item.serialize(),allusers))
-    request_body = json.loads(request.data)
-    results.append(request_body)
-    return jsonify(results), 201
+# @app.route('/user', methods=['POST'])
+# def add_new_user():
+#     allusers = User.query.all()
+#     results = list(map(lambda item: item.serialize(),allusers))
+#     request_body = json.loads(request.data)
+#     results.append(request_body)
+#     return jsonify(results), 201
 
 #DELETE
 #Delete user
@@ -166,6 +199,22 @@ def delete_favorite_planet(user_id, planet_id):
     favorites = Favorites.query.filter_by(userID=user_id).all()
     results = list(map(lambda item: item.serialize(),favorites))
     return jsonify(results), 200
+
+#SignUp route
+@app.route('/signup', methods=['POST'])
+def add_new_user():
+    request_body = json.loads(request.data)
+
+    new_user = User(email =  request_body['email'], userName = request_body['userName'], firstName = request_body['firstName'], lastName = request_body['lastName'], password = request_body['password'])
+    user = User.query.filter_by(email=request_body['email']).first()
+    if user is None:
+        db.session.add(new_user)
+        db.session.commit()
+        allusers = User.query.all()
+        results = list(map(lambda item: item.serialize(),allusers))
+        return jsonify(results), 201
+    
+    return jsonify("User already exists"), 400
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
